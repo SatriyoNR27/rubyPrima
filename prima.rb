cd34b55b17ee90d59fc0fc7950a8dacf2c07cd0c
Satriyo <satrio.nr27@gmail.com>
2020-02-20 09:45:20 +0700

    first commit

---
+++ prima.rb
@@ -0,0 +1,12 @@
+for angka in 2...1000
+    cek = 0
+    for x in 2..angka-1
+        if (angka % x == 0)
+    cek = 1
+        end
+    end
+
+    if (cek == 0)
+        puts angka
+        end
+end
